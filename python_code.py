d5d834a6e7ab1419084865b544ac0d13caf61d5e
navin trivedi <Navin Trivedi@r111028.ga1.swh.mhn.de>
2020-05-17 13:55:41 +0200

    capstone week-3 question 1

---
+++ python_code.py
@@ -0,0 +1,15 @@
+#This file contains first question of Capstone Week-3
+import pandas as pd 
+import requests 
+#import Numpy as np
+from bs4 import BeautifulSoup
+req = requests.get("https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M") 
+soup = BeautifulSoup(req.content,'lxml') 
+table = soup.find_all('table')[0] 
+df = pd.read_html(str(table))
+nh=pd.DataFrame(df[0])
+df2 = nh[nh['Borough'] == "Not assigned"].index
+nh.drop(df2, axis=0, inplace=True)
+nh.reset_index(drop=True, inplace=True)
+nh.shape
+n
