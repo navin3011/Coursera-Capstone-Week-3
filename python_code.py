d987567edbce2338ba244b75ece869f1d6686c09
navin3011 <36784413+navin3011@users.noreply.github.com>
2020-05-17 15:43:25 +0200

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -4,6 +4,7 @@
 from lxml.html import fromstring
 import pandas as pd 
 import requests 
+#import Numpy as np
 from bs4 import BeautifulSoup
 req = requests.get("https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M") 
 soup = BeautifulSoup(req.content,'lxml') 
@@ -13,5 +14,9 @@ nh=pd.DataFrame(df[0])
 df2 = nh[nh['Borough'] == "Not assigned"].index
 nh.drop(df2, axis=0, inplace=True)
 nh.reset_index(drop=True, inplace=True)
+nh.columns = ['Postcode','Borough', 'Neighbourhood']
+cols = nh.columns.tolist()
+cols
+nh= nh[cols]
 nh.shape
-n
+nh
